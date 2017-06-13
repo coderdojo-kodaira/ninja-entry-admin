fedb7b557405e213092378d2115c4a9e619e138f
Hiroshi Kumekawa <hkumekawa@gmail.com>
2017-06-13 21:53:23 +0900

    Login画面の見た目を調整。

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,7 +6,7 @@
           <div class="container section">
             <div class="has-text-right">
               <h1 class="title is-1">Login</h1> <br>
-              <p class="title is-3">Secure User Account Login</p>
+              <p class="title is-3">Coder Dojo Kodaira Administration System</p>
             </div>
           </div>
         </div>
@@ -19,7 +19,7 @@
       <section class="hero is-fullheight">
         <div class="hero-heading">
           <div class="section has-text-centered">
-            <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma logo" width="150px">
+            Coder Dojo Kodira<br>コーダー道場小平
           </div>
         </div>
         <div class="hero-body">
@@ -27,17 +27,17 @@
             <div class="columns">
               <div class="column is-8 is-offset-2">
                 <h1 class="avatar has-text-centered section">
-                  <img src="https://placehold.it/128x128">
+                  <img src="../assets/coderdojologo.png">
                 </h1>
                 <div class="login-form">
                   <p class="control has-icon has-icon-right">
-                    <input class="input email-input" type="text" placeholder="jsmith@example.org">
+                    <input class="input email-input" type="text" placeholder="id">
                     <span class="icon user">
                       <i class="fa fa-user"></i>
                     </span>
                   </p>
                   <p class="control has-icon has-icon-right">
-                    <input class="input password-input" type="password" placeholder="●●●●●●●">
+                    <input class="input password-input" type="password" placeholder="password">
                     <span class="icon user">
                       <i class="fa fa-lock"></i>
                     </span>
@@ -48,8 +48,6 @@
                 </div>
                 <div class="section forgot-password">
                   <p class="has-text-centered">
-                    <a href="#">Forgot password</a>
-                    <a href="#">Need help?</a>
                   </p>
                 </div>
               </div>
@@ -62,6 +60,7 @@
 </template>
 
 <script>
+
 </script>
 
 <style>
